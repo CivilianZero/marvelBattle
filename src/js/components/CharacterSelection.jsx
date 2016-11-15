7114a4ce0190f379067f31fa677485e14f723541
CivilianZero <Civilian.Protocol@gmail.com>
2016-11-15 13:19:10 -0500

    fixed broken character select image

--- src/js/components/CharacterSelection.jsx
+++ src/js/components/CharacterSelection.jsx
@@ -5,14 +5,15 @@ var battleStore = require('../stores/battleStore.js');
 var CharacterSelection = React.createClass({
 
 	render() {
-		var url = 'http://www.marvelsynergy.com/images/ultron.png',
-			divImage = {backgroundImage: 'url(' + url + ')'},
+		var url,
+			divImage,
 			wins = 0,
 			losses = 0,
 			hero = this.props.records.find((chara) => chara.name === this.props.name)
 		
 		if (this.props.id) {
 			url = this.props.image;
+			divImage = {backgroundImage: 'url(' + url + ')'};
 		}
 
 		if (hero){
