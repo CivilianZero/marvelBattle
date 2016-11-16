990ec2138d1f6f54f75065285f50b11fdddf6b9e
CivilianZero <Civilian.Protocol@gmail.com>
2016-11-16 17:01:01 -0500

    completed fade in

--- src/js/components/BattleView.jsx
+++ src/js/components/BattleView.jsx
@@ -1,29 +1,41 @@
 var React = require('react'),
 	TweenMax = require('gsap');
 
+// var BattleMessage = require('./BattleMessage.jsx');
+
 var BattleView = React.createClass({
 
 	render() {
 		var startFight,
 			fight,
 			count = 0,
-			message = 0;
-		if(this.props.narrative) {		
-			fight = this.props.narrative.fightData.map(function(turn) {
-				return <li className={'message' + (message++)} key={count++}>{turn.message}</li>
-			});
-			// for (var i = fight.length - 1; i >= 0; i--) {
-			// 	TweenMax.to('.message' + i, 10, {
-			// 		css: {
-			// 			opacity: '1'
-			// 		},
-			// 		delay: 2 * i,
-			// 	});
-			// }
-			// fight = fight.reverse();
-		}
+			index = 0;
+		
 		if(this.props.character1 && this.props.character2) {
 			startFight = this.props.children;
+		}
+
+		if(this.props.narrative) {		
+			fight = this.props.narrative.fightData.map(function(turn) {
+				return (
+					<li 
+						className='message'
+						id={'message' + index++}
+						key={count++}>
+						{turn.message}
+					</li>
+				)
+			});
+			for (var i = fight.length - 1; i >= 0; i--) {
+				TweenMax.to('#message' + i, 1, {
+					css: {
+						opacity: '1',
+						display: 'block'
+					},
+					delay: 2 * i
+				});
+			}
+			fight.reverse();
 		}
 		
 		return (
